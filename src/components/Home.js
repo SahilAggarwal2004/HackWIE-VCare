dd52f34a35256aeb13ddce7ca7fd5f6e336413e2
Sahil <sanjayaggarwal800@gmail.com>
2021-12-13 16:45:44 +0530

    second commit correction (final)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import Typed from "react-typed";
 
 export default function Home() {
+    const typed = ["Deforestation", "Pollution", "Global Warming", "Soil Degradation", "Waste Disposal", "And many more..."]
 
     return (
-        <div className="container" style={{marginTop:"20vh"}}>
+        <div className="container" style={{ marginTop: "20vh" }}>
             <h2>We help the World deal with problems like </h2>
-            <h1 style={{color:'green'}}><span className="auto-input"></span></h1>
+            <h1 style={{ color: 'green' }}>
+                <Typed strings={typed} typeSpeed={100} backSpeed={100} loop />
+            </h1>
             <button className="button">
                 <Link to="/solution">Let's Save the World &#10148;</Link>
             </button>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,17 +7,17 @@ export default function Navbar() {
         <nav className="navbar navbar-expand-lg navbar-light bg-transparent sticky-top">
             <div className="container-fluid">
                 <img src={logo} alt="" style={{ width: "3.5rem", marginRight: "2vw" }} />
-                <a className="navbar-brand text-center" href="/HackWIE-VCare">
+                <Link className="navbar-brand text-center" to="/">
                     <h4>VCare</h4>
                     <h6 className="my-0">We Care We Plant</h6>
-                </a>
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active navbtn" aria-current="page" href="/HackWIE-VCare">Home</a>
+                            <Link className="nav-link active navbtn" aria-current="page" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
                             <Link className="nav-link active navbtn" to="/threat">Threats</Link>
